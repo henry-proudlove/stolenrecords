b23c7643b0b7d17221ef582ff9bb85a50dc97b3f
Henry Proudlove <henry.proudlove@gmail.com>
2012-01-27 00:07:55 +0000

    Video page look nearly there

--- loop-media.php
+++ loop-media.php
@@ -7,16 +7,15 @@
 <?php /* Start the Loop */?>
 
 <div id="images"></div>
-<?php
-$art_args = array('post_type' => 'artist');
-$vid_arr = array();
 
-//$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+<?php //Getting data vid URLs from database
+
+$art_args = array('post_type' => 'artist' , 'posts_per_page' => '-1');
+$vid_arr = array;
 $art_query = new WP_Query($art_args);
 
 while ( $art_query->have_posts() ) : $art_query->the_post();
-	
-	_sr_post_header();
+
 	global $video_mb;
 	$meta = $video_mb->the_meta();
 	
@@ -24,52 +23,50 @@ while ( $art_query->have_posts() ) : $art_query->the_post();
 	{	
 		$video = $videos['video-link'];
 		if(!in_array($video , $vid_arr)){
-			array_push($vid_arr , $video);?>
-			<p><a href="<?php echo $video;?>" class="video"><?php echo $video; ?></a></p>
-		<?php }
+			array_push($vid_arr , $video);
+		}
 	}
 	
 	//Release sub query
 	$artist_tax = get_the_title();
-	$rel_args = array('post_type' => 'release' , 'artist' => $artist_tax);
+	$rel_args = array('post_type' => 'release' , 'artist' => $artist_tax , 'posts_per_page' => '-1');
 	$rel_query = new WP_query($rel_args);
-	
 	while ( $rel_query->have_posts() ) : $rel_query->the_post();
-		_sr_post_header();
 		global $video_mb;
 		$meta = $video_mb->the_meta();
-		
 		foreach ($meta['videos'] as $videos)
 		{	
 			$video = $videos['video-link'];
 			if(!in_array($video , $vid_arr)){
-				array_push($vid_arr , $video);?>
-				<p><a href="<?php echo $video;?>" class="video"><?php echo $video; ?></a></p>
-			<?php }
+				array_push($vid_arr , $video);
+			}
 		}
+		
 	endwhile;
+	
 endwhile;
-print_r($vid_arr);
-$endpoint_arr = array();
+
+//Constructing Endpoints from urls
+$vendors = array();
+$nodes = array();
+
 foreach ($vid_arr as $video)
 {	
 	if(strpos($video , 'vimeo.com'))
 	{
 		$video = substr($video , 17);
 		$api_endpoint = 'http://vimeo.com/api/v2/video/' . $video . '/videos.xml';
-		array_push($endpoint_arr, $api_endpoint);
-		
+		array_push($nodes, $api_endpoint);
+				
 	}elseif (strpos($video , 'youtu.be'))
 	{
 		$video = substr($video , 16);
 		$api_endpoint = 'http://gdata.youtube.com/feeds/api/videos/' . $video;
-		array_push($endpoint_arr, $api_endpoint);
+		array_push($nodes, $api_endpoint);
 	}
 }
-
 //Fetching the data
 
-$nodes = $endpoint_arr;
 $node_count = count($nodes);
 
 $curl_arr = array();
@@ -94,8 +91,19 @@ for($i = 0; $i < $node_count; $i++)
 	$vid_data[$i] = simplexml_load_string(curl_multi_getcontent  ( $curl_arr[$i]  ));
 }
 
-print_r($vid_data);
-$vid_data_count = count($vid_data);
-echo '</br>Video count: '.$vid_data_count.'</br>'
+print_r($vid_data[0]->video);
 
-?>
+for($i = 0; $i < $vendors_count; $i++)
+{	
+	$video = $vid_data[$i]->video ?>
+	<article class="media-thumb video">
+		<a>
+			<img src="<?php echo $video->thumbnail_large ?>" class="thumb-img" />
+			<div class="thumb-desc">
+				<h1 class="thumb-attr"><?php echo $video->title; ?> </h1>
+				<cite class="thumb-title"><?php echo strip_tags($video->description); ?></cite>
+			</div>
+		</a>
+	</article><!--.media-thumb-->
+<?php
+}?>
