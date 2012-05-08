5f9acb30c8ce4865e4e6df25276553c3cb5a6312
Henry Proudlove <henry.proudlove@gmail.com>
2012-05-08 22:09:48 +0100

    scroll events in script.js changing order of souncloud scripts loading seems to make it work in firefox

--- footer.php
+++ footer.php
@@ -12,10 +12,9 @@
 	var templateDir = "<?php bloginfo('template_directory') ?>";
 </script>
 <script src="<?php echo get_template_directory_uri();?>/js/jquery-ui-1.8.18.custom.min.js"></script>
-<script src="<?php echo get_template_directory_uri();?>/js/jquery.scroll.events.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/sc-player.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/soundcloud.player.api.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/scripts.js"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/soundcloud.player.api.js"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/sc-player.js"></script>
 
 <?php //Page conditional Scripts ?>
 
